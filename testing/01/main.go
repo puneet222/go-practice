e68b53d3585dbc429cb9b4be0d2cbb9ab671b440
puneet222 <aggarwal.puneet222@gmail.com>
2021-04-05 22:46:00 +0530

    BET

---
+++ testing/01/main.go
@@ -0,0 +1,20 @@
+package bet
+
+import (
+"fmt"
+	"github.com/puneet222/go-practice/testing/01/dog"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(10),
+	}
+	fmt.Println(fido)
+	fmt.Println(dog.YearsTwo(20))
+}
